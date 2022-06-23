6900aa0467fe41ca2f4b7ecdb51978ab8b679d83
Nahed Oukal <87930585+Nahed2000@users.noreply.github.com>
2022-06-23 07:55:31 +0300

    commit delete image

--- lib/screen/images_screen.dart
+++ lib/screen/images_screen.dart
@@ -1,5 +1,7 @@
 import 'package:api_first/api/api_settings.dart';
 import 'package:api_first/get/image_getx_controller.dart';
+import 'package:api_first/model/api_response.dart';
+import 'package:api_first/utils/Helper.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,7 +14,7 @@ class ImagesScreen extends StatefulWidget {
   State<ImagesScreen> createState() => _ImagesScreenState();
 }
 
-class _ImagesScreenState extends State<ImagesScreen> {
+class _ImagesScreenState extends State<ImagesScreen> with Helper{
   ImagesGetXController controller = Get.put(ImagesGetXController());
 
   @override
@@ -20,6 +22,9 @@ class _ImagesScreenState extends State<ImagesScreen> {
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
+          iconTheme: const IconThemeData(
+            color: Colors.black
+          ),
           actions: [
             IconButton(
               onPressed: () =>
@@ -41,10 +46,9 @@ class _ImagesScreenState extends State<ImagesScreen> {
           builder: (controller) {
             if (controller.loading.isTrue) {
               return const Center(
-                child:  CircularProgressIndicator(),
+                child: CircularProgressIndicator(),
               );
-            }
-            else if (controller.images.isNotEmpty) {
+            } else if (controller.images.isNotEmpty) {
               return GridView.builder(
                 padding: const EdgeInsets.all(20),
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -54,31 +58,71 @@ class _ImagesScreenState extends State<ImagesScreen> {
                 ),
                 itemCount: controller.images.length,
                 itemBuilder: (context, index) {
-                  return  Card(
+                  return Card(
                     clipBehavior: Clip.antiAlias,
                     elevation: 5,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                    child: Column(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Stack(
                       children: [
                         CachedNetworkImage(
+                          cacheKey: controller.images[index].image,
+                          width: double.infinity,
                           fit: BoxFit.cover,
-                          imageUrl: ApiSettings.imageApiUrl + controller.images[index].image,
-                          progressIndicatorBuilder: (context, url, downloadProgress) =>
-                              CircularProgressIndicator(value: downloadProgress.progress),
-                          errorWidget: (context, url, error) => const Icon(Icons.error),
+                          imageUrl: ApiSettings.imageApiUrl +
+                              controller.images[index].image,
+                          progressIndicatorBuilder:
+                              (context, url, downloadProgress) =>
+                                  Center(
+                                    child: CircularProgressIndicator(
+                                        value: downloadProgress.progress),
+                                  ),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
                         ),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            color: Colors.black45,
+                            height: 50,
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    controller.images[index].image,
+                                    style: GoogleFonts.nunito(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                    ),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () async=>await deleteImage(index: index),
+                                  icon: const Icon(
+                                    Icons.delete,
+                                    color: Colors.red,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   );
                 },
               );
-            }
-            else {
+            } else {
               return const Center(child: Text('No Data '));
             }
           },
         ));
+  }
+  Future<void>deleteImage({required int index})async{
+    ApiResponse apiResponse = await controller.deleteImage(index: index);
+    showSnackBar(context, message: 'Success Image Deleted ', status: !apiResponse.status);
   }
 }
